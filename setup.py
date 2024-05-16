f37a54ac658e12cbd4d340d47e20b646dc77b35a
MajidGazoum <146434549+MajidGazoum@users.noreply.github.com>
2024-05-16 03:00:29 +0200

    First Version

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="main.py",
+    version="1.0",
+    url="https://github.com/MajidGazoum/pythonProject",
+    author="majid_gazoum",
+    description="Projet Python",
+)
